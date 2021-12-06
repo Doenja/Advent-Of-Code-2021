1d5a5143891f42485cc0778ceba3572c42688173
marid00 <d.maris@belastingdienst.nl>
2021-12-06 19:38:49 +0100

    Start part1

--- src/dec3/part1.ts
+++ src/dec3/part1.ts
@@ -1,37 +1,34 @@
 import { readFileSync } from "fs";
 
-function getInput(file: string) {
-    const fileContent = readFileSync(file, "utf-8");
-
-    return fileContent.split("\n");
+export function getInput(file: string) {
+    return readFileSync(file, "utf-8")
+        .split("\n")
+        .map((number) => Array.from(number).map(Number));
 }
 
-export const binary = getInput("input/3.txt");
+export function getGamma(input: number[][]) {
+    const totals = new Array(5).fill(0);
 
-const length = binary[0].length;
-
-const collection = new Array(length).fill(0);
-
-function fillCollection() {
-    collection.forEach((_, i) => {
-        let total = 0;
-        binary.forEach((item) => {
-            if (item[i] === "1") {
-                total++;
-            }
+    input.forEach((arr) => {
+        arr.forEach((number, i) => {
+            totals[i] = totals[i] + number;
         });
+    });
 
-        collection[i] = total;
+    return totals.map((total) => {
+        return total / input.length > 0.5 ? 1 : 0;
     });
 }
 
-function getAnswer() {
-    fillCollection();
+function getEpsilon(gamma: number[]) {
+    return gamma.map((digit) => (digit === 1 ? 0 : 1));
+}
 
-    const gamma = collection.map((item) => (item / binary.length > 0.5 ? 1 : 0));
-    const epsilon = gamma.map((item) => (item === 1 ? 0 : 1));
+export function part1(file: string) {
+    const input = getInput(file);
+
+    const gamma = getGamma(input);
+    const epsilon = getEpsilon(gamma);
 
     return parseInt(gamma.join(""), 2) * parseInt(epsilon.join(""), 2);
 }
-
-export const part1 = getAnswer();
