4153dfe13421163e908d2580bd8f57f7f058ce6c
marid00 <d.maris@belastingdienst.nl>
2021-12-07 12:11:32 +0100

    Solve part 1

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import { part1 } from "./dec2/part1";
-import { part2 } from "./dec2/part2";
+import { part1 } from "./dec7/part1";
+// import { part2 } from "./dec7/part2";
 
-const day = 2;
+const day = 7;
 const testOutput = {
-    part1: 150,
-    part2: 900,
+    part1: 37,
+    part2: 0,
 };
 
 console.log("Test 1 is", part1(`input/${day}-test.txt`) === testOutput.part1 ? "a success!" : "failing");
-console.log("The answer to part 1 is", part1(`input/${day}.txt`));
+console.log("The a nswer to part 1 is", part1(`input/${day}.txt`));
 
-console.log("Test 2 is", part2(`input/${day}-test.txt`) === testOutput.part2 ? "a success!" : "failing");
-console.log("The answer to part 2 is", part2(`input/${day}.txt`));
+// console.log("Test 2 is", part2(`input/${day}-test.txt`) === testOutput.part2 ? "a success!" : "failing");
+// console.log("The answer to part 2 is", part2(`input/${day}.txt`));
--- src/dec7/part1.ts
+++ src/dec7/part1.ts
@@ -1,12 +1,24 @@
 import { readFileSync } from "fs";
 
 export function getInput(file: string) {
-    return readFileSync(file, "utf-8");
+    return readFileSync(file, "utf-8").split(",").map(Number);
+}
+
+export function getDistance(input: number[], from: number) {
+    let totalDistance = 0;
+
+    input.forEach((position) => (totalDistance += Math.abs(position - from)));
+    return totalDistance;
+}
+
+export function getFuel(input: number[]) {
+    const fuels = input.map((position) => getDistance(input, position));
+
+    return Math.min(...fuels);
 }
 
 export function part1(file: string) {
     const input = getInput(file);
-    console.log(input);
 
-    return 0;
+    return getFuel(input);
 }
