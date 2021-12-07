8320ef2a869188b0a957ae4d8f74ce4f2b92dcdd
marid00 <d.maris@belastingdienst.nl>
2021-12-07 14:06:34 +0100

    Solve part2

--- src/dec7/part1.ts
+++ src/dec7/part1.ts
@@ -4,15 +4,15 @@ export function getInput(file: string) {
     return readFileSync(file, "utf-8").split(",").map(Number);
 }
 
-export function getDistance(input: number[], from: number) {
+export function getDistance(input: number[], from: number, sumFormula: (a: number, b: number) => number) {
     let totalDistance = 0;
 
-    input.forEach((position) => (totalDistance += Math.abs(position - from)));
+    input.forEach((position) => (totalDistance += sumFormula(position, from)));
     return totalDistance;
 }
 
 export function getFuel(input: number[]) {
-    const fuels = input.map((position) => getDistance(input, position));
+    const fuels = input.map((position) => getDistance(input, position, (a, b) => Math.abs(a - b)));
 
     return Math.min(...fuels);
 }
--- src/dec7/part2.ts
+++ src/dec7/part2.ts
@@ -1,8 +1,29 @@
-import { getInput } from "./part1";
+import { readFileSync } from "fs";
+
+export function getInput(file: string) {
+    return readFileSync(file, "utf-8").split(",").map(Number);
+}
+
+export function getDistance(input: number[], from: number, sumFormula: (a: number, b: number) => number) {
+    let totalDistance = 0;
+
+    input.forEach((position) => (totalDistance += sumFormula(position, from)));
+    return totalDistance;
+}
+
+export function getFuel(input: number[]) {
+    const fuels = input.map((position) =>
+        getDistance(input, position, (a, b) => {
+            const n = Math.abs(a - b);
+            return (n * (n + 1)) / 2;
+        }),
+    );
+
+    return Math.min(...fuels);
+}
 
 export function part2(file: string) {
     const input = getInput(file);
-    console.log(input);
 
-    return 0;
+    return getFuel(input);
 }
