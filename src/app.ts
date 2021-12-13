4e384b049ede87cbf2fd2589751e116792be78a0
Doenja <doenjamaris@gmail.com>
2021-12-13 16:13:14 +0100

    Clean up dec3 part 1

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
-// import { part1 } from "./dec13/part1";
-import { part2 } from "./dec13/part2";
+import { part1 } from "./dec3/part1";
+// import { part2 } from "./dec3/part2";
 
-const day = 13;
+const day = 3;
 const testOutput = {
-    part1: 17,
-    part2: 0,
+    part1: 198,
+    // part2: 0,
 };
-// const output = {
-//     part1: part1(`input/${day}-test.txt`),
-//     part2: part2(`input/${day}-test.txt`),
-// };
+const output = {
+    part1: part1(`input/${day}-test.txt`),
+    // part2: part2(`input/${day}-test.txt`),
+};
 
-// console.log("Test 1 is", output.part1 === testOutput.part1 ? "a success!" : `failing with value ${output.part1}`);
-// console.log("The a nswer to part 1 is", part1(`input/${day}.txt`));
+console.log("Test 1 is", output.part1 === testOutput.part1 ? "a success!" : `failing with value ${output.part1}`);
+console.log("The a nswer to part 1 is", part1(`input/${day}.txt`));
 
-console.log("Test 2 is", part2(`input/${day}-test.txt`) === testOutput.part2 ? "a success!" : "failing");
-console.log("The answer to part 2 is", part2(`input/${day}.txt`));
+// console.log("Test 2 is", part2(`input/${day}-test.txt`) === testOutput.part2 ? "a success!" : "failing");
+// console.log("The answer to part 2 is", part2(`input/${day}.txt`));
--- src/dec3/part1.ts
+++ src/dec3/part1.ts
@@ -1,37 +1,31 @@
 import { readFileSync } from "fs";
 
-function getInput(file: string) {
-    const fileContent = readFileSync(file, "utf-8");
+export function getInput(file: string) {
+    const fileContent = readFileSync(file, "utf-8").trim();
+    const lines = fileContent.split("\n");
 
-    return fileContent.split("\n");
+    return lines.map((line) => line.split("").map(Number));
 }
 
-export const binary = getInput("input/3.txt");
-
-const length = binary[0].length;
-
-const collection = new Array(length).fill(0);
-
-function fillCollection() {
-    collection.forEach((_, i) => {
+export function getMostCommon(input: number[][]) {
+    let totals: number[] = [];
+    input[0].forEach((_, i) => {
         let total = 0;
-        binary.forEach((item) => {
-            if (item[i] === "1") {
-                total++;
-            }
+        input.forEach((number) => {
+            total += number[i];
         });
-
-        collection[i] = total;
+        totals = [...totals, total];
     });
+
+    return totals.map((total) => (total / input.length > 0.5 ? 1 : 0));
 }
 
-function getAnswer() {
-    fillCollection();
+export function part1(file: string) {
+    const input = getInput(file);
+    const mostCommon = getMostCommon(input);
 
-    const gamma = collection.map((item) => (item / binary.length > 0.5 ? 1 : 0));
-    const epsilon = gamma.map((item) => (item === 1 ? 0 : 1));
+    const gamma = mostCommon;
+    const epsilon = mostCommon.map((nr) => (nr === 1 ? 0 : 1));
 
     return parseInt(gamma.join(""), 2) * parseInt(epsilon.join(""), 2);
 }
-
-export const part1 = getAnswer();
